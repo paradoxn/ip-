d67b0a45f408b0d0a8da8c2f4f6b8dc8ed461ab5
paradoxn <paradoxn@163.com>
2019-11-09 14:28:55 +0800

    first get_proxy

--- estimate_ip.py
+++ estimate_ip.py
@@ -2,6 +2,11 @@ import requests
 import pymongo
 import threading
 import time
+import json
+
+lock = threading.Lock()
+
+
 
 def proxyisOK(proxy):  #判断ip是否可用
     try:
@@ -19,45 +24,58 @@ def proxyisOK(proxy):  #判断ip是否可用
     except:
         return False
 
-def updateMongo(i):  # 判断ip否可用，如果不可用则删除该ip。
-    global unuseful_count
+def updateMongo(i,collection):  # 判断ip否可用，如果不可用则从mongo删除该ip。
     host = i['host']
     port = i['port']
     proxy = host + ":" + str(port)
     if not proxyisOK(proxy):
         result = collection.delete_many({'host': host, 'port': port})
-        print('%s该IP不可用，将移除出数据库' % proxy)
-        unuseful_count += 1
         return result
     else:
+        now = time.time()
         result = collection.update_many({'host': host, 'port': port}, {'$set': {'timestamp': now}})
         return result
 
 
-if __name__ == "__main__":
-    unuseful_count = 0
-    start = time.time()
-    script_start = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start))
-    html = requests.get('https://www.baidu.com/s?wd=ip')
-    client = pymongo.MongoClient(host='127.0.0.1', port=27017)  # 连接远程数据库
-    db = client.paradata
-    db.authenticate('****', '****')  # 输入账号密码
-    collection = db.ip  # 选择table名"ip"
-    threads = []  # 多线程集合建立
-    now = time.time()
-    threedays_interval = now #- 259200
-    LL = collection.find({'timestamp': {'$lt': threedays_interval}})
-    check_count = collection.count_documents({'timestamp': {'$lt': threedays_interval}})
+def addMongo(i,collection):  #判断ip否可用，如果可用则上传至mongo数据库储存。i为代理的字典格式
+    if isinstance(i,str):
+        i=json.loads(i)
+    host=i['host']
+    port=i['port']
+    proxy=host+":"+str(port)
+    now=time.time()
+    times = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(now))
+    if collection.find_one({'host':host,'port':port}) is None and proxyisOK(proxy):  #如果ip可用且未重复，则将host和port加入数据库内
+        lock.acquire()
+        result=collection.insert_one({'host':i['host'],'port':i['port'],'country':i['country'],'type':i['type'],'anonymity':i['anonymity'],'check_time':times,'timestamp':now})
+        lock.release()
+        return result
 
-    for i in LL:  # 每隔0.1秒开启一个线程，分别传入updateMongo的参数为i
-        thread = threading.Thread(target=updateMongo, args=[i])
-        threads.append(thread)
-        thread.start()
-        time.sleep(0.1)
-    client.close()  # 断开数据库
 
-    for thread in threads:
-        thread.join()
-    end = time.time()
-    print('执行脚本时间：%s，验证mongo库存ip有效性脚本执行完毕，共计验证%s个库存ip，已清除不可用ip地址%s个。' % (
-    script_start, check_count, unuseful_count) + "总耗时:" + str(end - start) + "秒")
+# if __name__ == "__main__":
+#     unuseful_count = 0
+#     start = time.time()
+#     script_start = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start))
+#     html = requests.get('https://www.baidu.com/s?wd=ip')
+#     client = pymongo.MongoClient(host='127.0.0.1', port=27017)  # 连接远程数据库
+#     db = client.paradata
+#     db.authenticate('****', '****')  # 输入账号密码
+#     collection = db.ip  # 选择table名"ip"
+#     threads = []  # 多线程集合建立
+#     now = time.time()
+#     threedays_interval = now - 259200
+#     LL = collection.find({'timestamp': {'$lt': threedays_interval}})
+#     check_count = collection.count_documents({'timestamp': {'$lt': threedays_interval}})
+#
+#     for i in LL:  # 每隔0.1秒开启一个线程，分别传入updateMongo的参数为i
+#         thread = threading.Thread(target=updateMongo, args=[i])
+#         threads.append(thread)
+#         thread.start()
+#         time.sleep(0.1)
+#     client.close()  # 断开数据库
+#
+#     for thread in threads:
+#         thread.join()
+#     end = time.time()
+#     print('执行脚本时间：%s，验证mongo库存ip有效性脚本执行完毕，共计验证%s个库存ip，已清除不可用ip地址%s个。' % (
+#     script_start, check_count, unuseful_count) + "总耗时:" + str(end - start) + "秒")
--- main.py
+++ main.py
@@ -1,69 +1,61 @@
 import requests
-import json
 import pymongo
 import threading
 import time
 import proxies_web
+from estimate_ip import updateMongo,addMongo
 
-def proxyisOK(proxy):  #判断ip是否可用
-    try:
-        requests.get('https://www.baidu.com/s?wd=ip',headers={'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'})
-    except:
-        return True
-    proxies={
-        'http':proxy,
-        'https':proxy
-    }
-    try:
-        html=requests.get('https://www.baidu.com/s?wd=ip',proxies=proxies,timeout=20,headers={'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'})
-        if html.status_code==200:
-            return True
-    except:
-        return False
-
-
-def addMongo(i):  #判断ip否可用，如果可用则上传至数据库储存。i为代理的字典格式
-    global useful_count
-    if isinstance(i,str):
-        i=json.loads(i)
-   # print('正在验证ip:%s'%i)
-    host=i['host']
-    port=i['port']
-    proxy=host+":"+str(port)
-    now=time.time()
-    times = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(now))
-    if collection.find_one({'host':host,'port':port}) is None and proxyisOK(proxy):  #如果ip可用且未重复，则将host和port加入数据库内
-        lock.acquire()
-        result=collection.insert_one({'host':i['host'],'port':i['port'],'country':i['country'],'type':i['type'],'anonymity':i['anonymity'],'check_time':times,'timestamp':now})
-        useful_count += 1
-        lock.release()
-        return result
 
 
 if __name__=="__main__":
     start = time.time()
     script_start = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start))
     lock = threading.Lock()
-    client=pymongo.MongoClient(host='127.0.0.1',port=27017)  #连接远程mongo数据库
+    client=pymongo.MongoClient(host='34.92.209.10',port=27017)  #连接远程mongo数据库
     db=client.paradata #选择数据库
-    db.authenticate('****','****')  #输入账号密码
+    db.authenticate('para','2415')  #输入账号密码
     collection=db.ip  #选择table名"ip"
     threads=[]  #多线程集合建立
     LSS = proxies_web.Proxies()
     LL = LSS.prox_list
     all_ip_count = LSS.count
-    useful_count = 0
     print('开始对ip进行验证')
     for i in LL:  #每隔0.1秒开启一个线程，分别传入addMongo的参数为i
-        thread=threading.Thread(target=addMongo,args=[i])
+        thread=threading.Thread(target=addMongo,args=[i,collection])
         threads.append(thread)
         thread.start()
         time.sleep(0.1)
-
-    client.close()  #断开数据库
-
+#    client.close()  #断开数据库
     for thread in threads:
         thread.join()
-
     end = time.time()
-    print('执行脚本时间：%s，获取ip并导入mongo脚本执行完毕，共计验证%s个ip，实际新增可用ip地址%s个。'%(script_start,all_ip_count,useful_count) + "总耗时:" + str(end - start) + "秒")
+    print('执行脚本时间：%s，获取ip并导入mongo脚本执行完毕，共计验证%s个ip。'%(script_start,all_ip_count) + "总耗时:" + str(end - start) + "秒")
+#-------------------------------------------------------------------------------------------------------------------
+
+    print('目前库存ip，开始执行库存ip验证情况：')
+    start_all_count = collection.count_documents({})
+    time.sleep(10)
+    start2 = time.time()
+    script_start2 = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start))
+    html = requests.get('https://www.baidu.com/s?wd=ip')
+    # client = pymongo.MongoClient(host='34.92.209.10', port=27017)  # 连接远程数据库
+    # db = client.paradata
+    # db.authenticate('para', '2415')  # 输入账号密码
+    # collection = db.ip  # 选择table名"ip"
+    threads = []  # 多线程集合建立
+    now = time.time()
+    threedays_interval = now# - 259200
+    LL = collection.find({'timestamp': {'$lt': threedays_interval}})
+    check_count = collection.count_documents({'timestamp': {'$lt': threedays_interval}})
+    for i in LL:  # 每隔0.1秒开启一个线程，分别传入updateMongo的参数为i
+        thread = threading.Thread(target=updateMongo, args=[i,collection])
+        threads.append(thread)
+        thread.start()
+        time.sleep(0.1)
+    for thread in threads:
+        thread.join()
+    end_all_count = collection.count_documents({})
+    client.close()  # 断开数据库
+    end2 = time.time()
+    print('执行脚本时间：%s，验证mongo库存ip有效性脚本执行完毕，共计验证%s个库存ip，删除%s个无效ip，剩余IP总数%s个。' % (
+    script_start2, check_count,start_all_count-end_all_count,end_all_count) + "总耗时:" + str(end2 - start2) + "秒")
